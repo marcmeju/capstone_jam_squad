ac5386c4f3a36f29c60aa12c18c1c8847717d09d
Jeya Preetha Ganesh Kumar <jkumar@dev-10.com>
2022-01-03 14:35:48 -0600

    adding schema and ddl

---
+++ src/sql/DDL.sql
@@ -0,0 +1,216 @@
+-- -----------------------------------------------------
+-- Schema not_booking
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `not_booking` ;
+
+-- -----------------------------------------------------
+-- Schema not_booking
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `not_booking` DEFAULT CHARACTER SET utf8 ;
+USE `not_booking` ;
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Location` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Location` (
+  `location_id` INT NOT NULL AUTO_INCREMENT,
+  `location_city` VARCHAR(45) NOT NULL,
+  `location_state` VARCHAR(45) NULL,
+  `location_address` VARCHAR(45) NULL,
+  `location_zipcode` INT NULL,
+  PRIMARY KEY (`location_id`),
+  UNIQUE INDEX `location_name_UNIQUE` (`location_city` ASC) VISIBLE)
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Tier`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Tier` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Tier` (
+  `tier_id` INT NOT NULL AUTO_INCREMENT,
+  `tier_name` VARCHAR(45) NULL,
+  `tier_desc` VARCHAR(45) NULL,
+  PRIMARY KEY (`tier_id`))
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Package`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Package` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Package` (
+  `package_id` INT NOT NULL AUTO_INCREMENT,
+  `package_name` VARCHAR(45) NULL,
+  `tier_id` INT NULL,
+  `package_price` INT NULL,
+  PRIMARY KEY (`package_id`),
+  INDEX `tier_id_idx` (`tier_id` ASC) VISIBLE,
+  CONSTRAINT `tier_id`
+    FOREIGN KEY (`tier_id`)
+    REFERENCES `not_booking`.`Tier` (`tier_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Event`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Event` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Event` (
+  `event_id` INT NOT NULL,
+  `event_type` VARCHAR(45) NOT NULL,
+  `event_name` VARCHAR(45) NOT NULL,
+  `event_date` DATETIME NOT NULL,
+  `event_price` INT NULL,
+  `location_id` INT NULL,
+  PRIMARY KEY (`event_id`),
+  UNIQUE INDEX `event_type_UNIQUE` (`event_type` ASC) VISIBLE,
+  UNIQUE INDEX `event_name_UNIQUE` (`event_name` ASC) VISIBLE,
+  INDEX `location_id_idx` (`location_id` ASC) VISIBLE,
+  CONSTRAINT `location_id`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `not_booking`.`Location` (`location_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`user_role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`user_role` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`user_role` (
+  `user_role_id` INT NOT NULL AUTO_INCREMENT,
+  `role_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`user_role_id`))
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NULL,
+  `password_hash` VARCHAR(100) NULL,
+  `user_role_id` INT NULL,
+  PRIMARY KEY (`user_id`),
+  INDEX `user_role_id_idx` (`user_role_id` ASC) VISIBLE,
+  CONSTRAINT `user_role_id`
+    FOREIGN KEY (`user_role_id`)
+    REFERENCES `not_booking`.`user_role` (`user_role_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Customer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Customer` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Customer` (
+  `customer_id` INT NOT NULL AUTO_INCREMENT,
+  `customer_first_name` VARCHAR(45) NULL,
+  `customer_last_name` VARCHAR(45) NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE INDEX `customer_id_UNIQUE` (`customer_id` ASC) VISIBLE,
+  INDEX `fk_Customer_user1_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Customer_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `not_booking`.`user` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Booking`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Booking` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Booking` (
+  `booking_id` INT NOT NULL AUTO_INCREMENT,
+  `package_id` INT NULL,
+  `customer_id` INT NULL,
+  PRIMARY KEY (`booking_id`),
+  UNIQUE INDEX `booking_id_UNIQUE` (`booking_id` ASC) VISIBLE,
+  INDEX `package_id_idx` (`package_id` ASC) VISIBLE,
+  INDEX `customer_id_idx` (`customer_id` ASC) VISIBLE,
+  CONSTRAINT `package_id`
+    FOREIGN KEY (`package_id`)
+    REFERENCES `not_booking`.`Package` (`package_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `customer_id`
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `not_booking`.`Customer` (`customer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Package_event`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Package_event` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Package_event` (
+  `package_event_id` INT NOT NULL,
+  `event_id` INT NULL,
+  `package_id` INT NOT NULL,
+  PRIMARY KEY (`package_event_id`),
+  UNIQUE INDEX `package_id_UNIQUE` (`package_event_id` ASC) VISIBLE,
+  INDEX `fk_Package_event_Package_idx` (`package_id` ASC) VISIBLE,
+  INDEX `event_id_idx` (`event_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Package_event_Package`
+    FOREIGN KEY (`package_id`)
+    REFERENCES `not_booking`.`Package` (`package_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `event_id`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `not_booking`.`Event` (`event_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- -----------------------------------------------------
+-- Table `not_booking`.`Contact`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `not_booking`.`Contact` ;
+
+CREATE TABLE IF NOT EXISTS `not_booking`.`Contact` (
+  `contact_id` INT NOT NULL AUTO_INCREMENT,
+  `customer_id` INT NULL,
+  `event_id` INT NULL,
+  `phone` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `contact_type` VARCHAR(45) NULL,
+  PRIMARY KEY (`contact_id`),
+  UNIQUE INDEX `contact_id_UNIQUE` (`contact_id` ASC) VISIBLE,
+  INDEX `event_id_idx` (`event_id` ASC) VISIBLE,
+  INDEX `customer_id_idx` (`customer_id` ASC) VISIBLE,
+  CONSTRAINT `event_id`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `not_booking`.`Event` (`event_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `customer_id`
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `not_booking`.`Customer` (`customer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
---
+++ tasks.md
@@ -0,0 +1,31 @@
+1.	Create Data layer
+1.1.	Create a database in MySql.
+1.2.	Define Schema for the travel data tables.
+1.3.	Define Schema for the bridge table.
+1.4.	Define Schema for user table
+1.5.	Create the DDL for the defined schemas
+1.6.	Create and load sample data for all the tables
+2.	Create Java Microservice
+2.1.	Create the Data Layer and Mappers
+2.2.	Create the Domain Layer
+2.3.	Create the Model Layer
+2.4.	Create the Controllers
+2.5.	Implement Spring Boot framework
+2.6.	Create Unit Testing for each layer
+2.7.	Create http endpoint details
+3.	Create React App
+3.1.	Define the Login page
+3.2.	Define the View package page
+3.3.	Define the Create booking page
+3.4.	Define the Edit booking page
+3.5.	Define the delete booking page 
+3.6.	Create Admin Create page to create packages, add locations, and add events
+3.7.	Create Admin edit page to edit prices and dates
+3.8.	Create Admin delete page delete packages, locations, and events
+4.	Create Powerpoint presentation
+4.1.	Create Team members detail page
+4.2.	Create project summary page
+4.3.	Create architecture diagram page
+4.4.	Create DB/Table details page
+4.5.	Create java/API endpoint details page
+4.6.	Create React/functional pages details
