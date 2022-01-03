6380fa7e882ce849cd5d1167e163d8717183968b
VishalT <VTomby@dev-10.com>
2022-01-03 16:50:49 -0500

    Skeleton setup of files we will need

---
+++ src/react/notbooking-capstone/src/Components/Errors.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Errors({ errors }) {
+  if (errors.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="alert alert-danger">
+      The following errors were found:
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Errors;
