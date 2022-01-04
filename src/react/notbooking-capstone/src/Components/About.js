4ec74281655b5e919649e700df7392120f76552a
VishalT <VTomby@dev-10.com>
2022-01-04 11:00:12 -0500

    About and contact pages are filled

---
+++ src/react/notbooking-capstone/src/Components/About.js
@@ -0,0 +1,10 @@
+function About(){
+    return(
+        <main>
+            <h2>About</h2>
+            <p>NotBooking helps you make your vacations easier. Choose from one of our package options to find 
+                events in one of our locations NYC , Chicago, or LA!!!
+            </p>
+        </main>
+    )
+}
