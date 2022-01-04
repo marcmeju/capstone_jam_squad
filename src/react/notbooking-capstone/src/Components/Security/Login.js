f9752c17a0324c3a883676a988d333980dc366aa
VishalT <VTomby@dev-10.com>
2022-01-04 10:55:39 -0500

    Login and Register pages are filled with relevant forms

---
+++ src/react/notbooking-capstone/src/Components/Security/Login.js
@@ -0,0 +1,88 @@
+import { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import AuthContext from '../AuthContext';
+
+import Errors from './Errors';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const auth = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const usernameOnChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordOnChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const authAttempt = {
+      username,
+      password
+    };
+
+    const init = {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(authAttempt)
+    };
+
+    fetch('http://localhost:5000/authenticate', init)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else if (response.status === 403) {
+          return null;
+        }
+        return Promise.reject('Something unexpected went wrong :)');
+      })
+      .then(data => {
+        if (data) {
+          auth.login(data.jwt_token);
+          history.push('/');
+        } else {
+          
+          setErrors(['Login failure.']);
+        }
+      })
+      .catch(error => console.log(error));
+  };
+
+  return (
+    <>
+      <h2 className="my-4">Login</h2>
+      <Errors errors={errors} />
+      <form onSubmit={formSubmitHandler}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input className="form-control" type="text" id="username" name="username" 
+            value={username} onChange={usernameOnChangeHandler} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input className="form-control" type="password" id="password" name="password" 
+            value={password} onChange={passwordOnChangeHandler} />
+        </div>
+        <div className="mt-5">
+          <button className="btn btn-success" type="submit">
+            <i className="bi bi-plus-circle-fill"></i> Login</button>
+          <Link to="/" className="btn btn-warning ml-2">
+            <i className="bi bi-x"></i> Cancel
+          </Link>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/react/notbooking-capstone/src/Components/Security/Register.js
@@ -0,0 +1,129 @@
+import { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import AuthContext from '../AuthContext';
+
+import Errors from `../Errors`
+
+function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const auth = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const usernameOnChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordOnChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const confirmPasswordOnChangeHandler = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    
+    setErrors([]);
+
+    if (password !== confirmPassword) {
+      setErrors(['Password and confirm password don\'t match']);
+      return;
+    }
+
+    const newUser = {
+      username,
+      password
+    };
+
+    const init = {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    };
+
+    fetch('http://localhost:5000/create_account', init)
+      .then(response => {
+        if (response.status === 201 || response.status === 400) {
+          return response.json();
+        }
+        return Promise.reject('Something unexpected went wrong :)');
+      })
+      .then(data => {
+        if (data.id) {
+          const init = {
+            method: 'POST', 
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+          };
+      
+          fetch('http://localhost:5000/authenticate', init)
+            .then(response => {
+              if (response.status === 200) {
+                return response.json();
+              } else if (response.status === 403) {
+                return null;
+              }
+              return Promise.reject('Something unexpected went wrong :)');
+            })
+            .then(data => {
+              if (data) {
+                auth.login(data.jwt_token);
+                history.push('/');
+              } else {
+                
+                setErrors(['Login failure.']);
+              }
+            })
+            .catch(error => console.log(error));
+        } else {
+          
+          setErrors(data.messages)
+        }
+      })
+      .catch(error => console.log(error));
+  };
+
+  return (
+    <>
+      <h2 className="my-4">Register</h2>
+      <Errors errors={errors} />
+      <form onSubmit={formSubmitHandler}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input className="form-control" type="text" id="username" name="username" 
+            value={username} onChange={usernameOnChangeHandler} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input className="form-control" type="password" id="password" name="password" 
+            value={password} onChange={passwordOnChangeHandler} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input className="form-control" type="password" id="confirmPassword" name="confirmPassword" 
+            value={confirmPassword} onChange={confirmPasswordOnChangeHandler} />
+        </div>
+        <div className="mt-5">
+          <button className="btn btn-success" type="submit">
+            <i className="bi bi-plus-circle-fill"></i> Register</button>
+          <Link to="/" className="btn btn-warning ml-2">
+            <i className="bi bi-x"></i> Cancel
+          </Link>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Register;
